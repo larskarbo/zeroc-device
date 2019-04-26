ea55547c026d81ccf85c02d7d55cdb137106e8eb
Lars Karbo <mail@larskarbo.no>
2019-04-26 15:42:54 +0200

    upd

--- CameraMan.js
+++ CameraMan.js
@@ -3,7 +3,6 @@ var sys = require('sys')
 var util = require('util')
 const exec = util.promisify(require('child_process').exec);
 
-const util = require('util');
 
 class CameraMan {
     constructor() {
