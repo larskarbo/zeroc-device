71edde8976a41456daa1018bee1e62d199b1ee95
Lars Karbo <mail@larskarbo.no>
2019-04-26 15:24:47 +0200

    upd

--- CameraMan.js
+++ CameraMan.js
@@ -1,4 +1,6 @@
 
+var sys = require('sys')
+var exec = require('child_process').exec;
 
 class CameraMan {
     constructor() {
@@ -10,10 +12,24 @@ class CameraMan {
     }
 
     onChange(callback){
-        setInterval(() => {
-            callback(this.isRecording)
-            this.isRecording = !this.isRecording
-        }, 1000)
+        // setInterval(() => {
+        //     callback(this.isRecording)
+        //     this.isRecording = !this.isRecording
+        // }, 1000)
+    }
+
+    startRecording() {
+        this.isRecording = true
+
+        const cmd = `raspivid -o video.h264 -t 10000`
+        
+        child = exec(cmd, function (error, stdout, stderr) {
+            sys.print('stdout: ' + stdout);
+            sys.print('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
     }
 }
 
